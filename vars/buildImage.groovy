40e7f9a5205bee59b52769f46805425ae5b9a76e
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-11-25 09:14:36 +0200

    Change docker image version from jma-2.0 to jma-3.1

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,8 +2,8 @@
 def call() {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t malware4/java-maven-app:jma-2.0 .'
+        sh 'docker build -t malware4/java-maven-app:jma-3.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push malware4/java-maven-app:jma-2.0'
+        sh 'docker push malware4/java-maven-app:jma-3.1'
     }
 }
