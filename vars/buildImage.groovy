27364ffc453ba4b8968942f804be4ee7e4fb1770
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-11-25 11:20:06 +0200

    Change buildImage function to take the imageName as parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/user/bin/env groovy
-def call() {
+def call(String imageName) {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t malware4/java-maven-app:jma-3.1 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push malware4/java-maven-app:jma-3.1'
+        sh "docker push $imageName"
     }
 }
