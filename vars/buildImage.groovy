dd8316cbdf926fabe62b2449adf6daafc2073e22
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-11-25 11:02:14 +0200

    Push new docker image version

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,8 +2,8 @@
 def call() {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t malware4/java-maven-app:jma-3.1 .'
+        sh 'docker build -t malware4/java-maven-app:jma-3.2 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push malware4/java-maven-app:jma-3.1'
+        sh 'docker push malware4/java-maven-app:jma-3.2'
     }
 }
