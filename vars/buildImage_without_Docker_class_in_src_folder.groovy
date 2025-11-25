7efdbd9a16aaa6f0adff26269f1f77ff48d69653
Nikolaos Zerzelidis <nickzerze@gmail.com>
2025-11-25 14:00:58 +0200

    Split buildDockerImage into seperate steps

---
+++ vars/buildImage_without_Docker_class_in_src_folder.groovy
@@ -0,0 +1,9 @@
+#!/user/bin/env groovy
+def call(String imageName) {
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t $imageName ."
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh "docker push $imageName"
+    }
+}
